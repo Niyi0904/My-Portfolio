4a8d78bdd2fd3c2b4fea33c6f19b3a62af8364b3
Niyi0904 <owoyeminiyi2@gmail.com>
2024-06-19 20:56:01 +0100

    ggg

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,26 @@
+import image from '../images/myImage.jpg'
+
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hello World, i am Niyi</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Frontend</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            <br /> Dolorum, quas. Amet soluta assumenda cum?
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src={image} alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
