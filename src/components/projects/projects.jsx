bdced069fb0920b962c9dccd617c5025ab87492d
Niyi0904 <owoyeminiyi2@gmail.com>
2024-10-06 04:54:09 +0300

    adjusted for xs and sm media queries

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => (
         <h1 className="text-4xl font-extrabold flex justify-center mb-4">PROJECTS</h1>
         <div className="flex flex-wrap px-4 gap-x-12 pt-10 justify-center items-center w-full text-blue-950">
             {projectData.map(projects  => (       
-                <div className="bg-white project-card w-80 xs:w-[80%] sm:w-[80%] h-[400px] rounded-md xs:mt-8 sm:mt-8 mt-4">
+                <div className="bg-white project-card w-80 xs:w-[90%] sm:w-[90%] h-[400px] rounded-md xs:mt-8 sm:mt-8 mt-4">
                     <div className="relative mb-1 mt-2 left-5 h-8">
                         <h1 className="text-xl font-medium project-title">{projects.projectTitle}</h1>
                         <i className="fi fi-brands-github text-3xl text-right relative left-[80%] bottom-7"></i>
