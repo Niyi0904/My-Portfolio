6f3efafffd62c79c40cba859253dcf0f15074d77
Niyi0904 <owoyeminiyi2@gmail.com>
2024-10-06 04:50:43 +0300

    testing media Queries and added github logo

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -9,13 +9,14 @@ const Projects = () => (
         <h1 className="text-4xl font-extrabold flex justify-center mb-4">PROJECTS</h1>
         <div className="flex flex-wrap px-4 gap-x-12 pt-10 justify-center items-center w-full text-blue-950">
             {projectData.map(projects  => (       
-                <div className="bg-white project-card w-80 xs:w-96 sm:w-96 h-[400px] rounded-md xs:mt-8 sm:mt-8 mt-4">
-                    <div className="relative mb-1 mt-2 left-5">
+                <div className="bg-white project-card w-80 xs:w-[60%] sm:w-[80%] h-[400px] rounded-md xs:mt-8 sm:mt-8 mt-4">
+                    <div className="relative mb-1 mt-2 left-5 h-8">
                         <h1 className="text-xl font-medium project-title">{projects.projectTitle}</h1>
+                        <i className="fi fi-brands-github text-3xl text-right relative left-[80%] bottom-7"></i>
                     </div>
                     <div className="w-full flex h-[70%] justify-center items-center pt-0">
                         <img className="relative w-[95%] rounded-sm h-[95%] project-image" src={projects.projectImage}/>
-                    </div>
+                    </div>  
                     <div className="relative left-4">
                         <h1 className="relative top-0 font-medium project-stack text-md italic text-gray-500">{projects.projectStack}</h1>
                     </div>
